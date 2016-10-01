6a7b8e5eb7a065887e008e34ed69a39352940ad6
Maxim Romanov <romanov.maxim@gmail.com>
2016-10-01 14:27:27 +0200

    testing data for 4 books

--- run_tests.sh
+++ run_tests.sh
@@ -6,75 +6,76 @@ echo $SCRIPTPATH/$1/
 
 # creating manifests
 cd $SCRIPTPATH/$1/7_final/ && ls *.png > manifest.txt
+cd $SCRIPTPATH/$1/7_final_200/ && ls *.png > manifest.txt
 cd $SCRIPTPATH/$1/7_final_a/ && ls *.png > manifest.txt
 cd $SCRIPTPATH/$1/7_final_b/ && ls *.png > manifest.txt
 cd $SCRIPTPATH/$1/7_final_a_200/ && ls *.png > manifest.txt
 cd $SCRIPTPATH/$1/7_final_b_200/ && ls *.png > manifest.txt
 
 # run a
-VAR="a"
-FOLDER="7_final_"$VAR
+VAR="_a"
+FOLDER="7_final"$VAR
 MODEL="~/.config/kraken/arabic-beirut.clstm"
 
 python $SCRIPTPATH/eval.py $MODEL $SCRIPTPATH/$1/$FOLDER
-cat $SCRIPTPATH/$1/$FOLDER/*.gt.txt > $SCRIPTPATH/$1/g_$VAR.txt
-cat $SCRIPTPATH/$1/$FOLDER/*.rec.txt > $SCRIPTPATH/$1/r_$VAR.txt
-ocrevalutf8 accuracy $SCRIPTPATH/$1/g_$VAR.txt $SCRIPTPATH/$1/r_$VAR.txt > $SCRIPTPATH/$1/report_$VAR.txt
+cat $SCRIPTPATH/$1/$FOLDER/*.gt.txt > $SCRIPTPATH/$1/g$VAR.txt
+cat $SCRIPTPATH/$1/$FOLDER/*.rec.txt > $SCRIPTPATH/$1/r$VAR.txt
+ocrevalutf8 accuracy $SCRIPTPATH/$1/g$VAR.txt $SCRIPTPATH/$1/r$VAR.txt > $SCRIPTPATH/$1/report$VAR.txt
 echo $SCRIPTPATH/$1/$FOLDER": folder has been processed!"
 
 # run b
-VAR="b"
-FOLDER="7_final_"$VAR
+VAR="_b"
+FOLDER="7_final"$VAR
 MODEL="~/.config/kraken/arabic-beirut.clstm"
 
 python $SCRIPTPATH/eval.py $MODEL $SCRIPTPATH/$1/$FOLDER
-cat $SCRIPTPATH/$1/$FOLDER/*.gt.txt > $SCRIPTPATH/$1/g_$VAR.txt
-cat $SCRIPTPATH/$1/$FOLDER/*.rec.txt > $SCRIPTPATH/$1/r_$VAR.txt
-ocrevalutf8 accuracy $SCRIPTPATH/$1/g_$VAR.txt $SCRIPTPATH/$1/r_$VAR.txt > $SCRIPTPATH/$1/report_$VAR.txt
+cat $SCRIPTPATH/$1/$FOLDER/*.gt.txt > $SCRIPTPATH/$1/g$VAR.txt
+cat $SCRIPTPATH/$1/$FOLDER/*.rec.txt > $SCRIPTPATH/$1/r$VAR.txt
+ocrevalutf8 accuracy $SCRIPTPATH/$1/g$VAR.txt $SCRIPTPATH/$1/r$VAR.txt > $SCRIPTPATH/$1/report$VAR.txt
 echo $SCRIPTPATH/$1/$FOLDER": folder has been processed!"
 
 # run a_200
-VAR="a_200"
-FOLDER="7_final_"$VAR
+VAR="_a_200"
+FOLDER="7_final"$VAR
 MODEL="~/.config/kraken/arabic-beirut-200.clstm"
 
 python $SCRIPTPATH/eval.py $MODEL $SCRIPTPATH/$1/$FOLDER
-cat $SCRIPTPATH/$1/$FOLDER/*.gt.txt > $SCRIPTPATH/$1/g_$VAR.txt
-cat $SCRIPTPATH/$1/$FOLDER/*.rec.txt > $SCRIPTPATH/$1/r_$VAR.txt
-ocrevalutf8 accuracy $SCRIPTPATH/$1/g_$VAR.txt $SCRIPTPATH/$1/r_$VAR.txt > $SCRIPTPATH/$1/report_$VAR.txt
+cat $SCRIPTPATH/$1/$FOLDER/*.gt.txt > $SCRIPTPATH/$1/g$VAR.txt
+cat $SCRIPTPATH/$1/$FOLDER/*.rec.txt > $SCRIPTPATH/$1/r$VAR.txt
+ocrevalutf8 accuracy $SCRIPTPATH/$1/g$VAR.txt $SCRIPTPATH/$1/r$VAR.txt > $SCRIPTPATH/$1/report$VAR.txt
 echo $SCRIPTPATH/$1/$FOLDER": folder has been processed!"
 
 # run b_200
-VAR="b_200"
-FOLDER="7_final_"$VAR
+VAR="_b_200"
+FOLDER="7_final"$VAR
 MODEL="~/.config/kraken/arabic-beirut-200.clstm"
 
 python $SCRIPTPATH/eval.py $MODEL $SCRIPTPATH/$1/$FOLDER
-cat $SCRIPTPATH/$1/$FOLDER/*.gt.txt > $SCRIPTPATH/$1/g_$VAR.txt
-cat $SCRIPTPATH/$1/$FOLDER/*.rec.txt > $SCRIPTPATH/$1/r_$VAR.txt
-ocrevalutf8 accuracy $SCRIPTPATH/$1/g_$VAR.txt $SCRIPTPATH/$1/r_$VAR.txt > $SCRIPTPATH/$1/report_$VAR.txt
+cat $SCRIPTPATH/$1/$FOLDER/*.gt.txt > $SCRIPTPATH/$1/g$VAR.txt
+cat $SCRIPTPATH/$1/$FOLDER/*.rec.txt > $SCRIPTPATH/$1/r$VAR.txt
+ocrevalutf8 accuracy $SCRIPTPATH/$1/g$VAR.txt $SCRIPTPATH/$1/r$VAR.txt > $SCRIPTPATH/$1/report$VAR.txt
 echo $SCRIPTPATH/$1/$FOLDER": folder has been processed!"
 
 # run 0 --- may not work (too large)
-VAR="all"
+VAR="_all"
 FOLDER="7_final"
 MODEL="~/.config/kraken/arabic-beirut.clstm"
 
 python $SCRIPTPATH/eval.py $MODEL $SCRIPTPATH/$1/$FOLDER
-cat $SCRIPTPATH/$1/$FOLDER/*.gt.txt > $SCRIPTPATH/$1/g_$VAR.txt
-cat $SCRIPTPATH/$1/$FOLDER/*.rec.txt > $SCRIPTPATH/$1/r_$VAR.txt
-ocrevalutf8 accuracy $SCRIPTPATH/$1/g_$VAR.txt $SCRIPTPATH/$1/r_$VAR.txt > $SCRIPTPATH/$1/report_$VAR.txt
+cat $SCRIPTPATH/$1/$FOLDER/*.gt.txt > $SCRIPTPATH/$1/g$VAR.txt
+cat $SCRIPTPATH/$1/$FOLDER/*.rec.txt > $SCRIPTPATH/$1/r$VAR.txt
+ocrevalutf8 accuracy $SCRIPTPATH/$1/g$VAR.txt $SCRIPTPATH/$1/r$VAR.txt > $SCRIPTPATH/$1/report$VAR.txt
 echo $SCRIPTPATH/$1/$FOLDER": folder has been processed!"
 
 # run 0-200 --- may not work (too large)
-VAR="all_200"
-FOLDER="7_final"
+VAR="_all_200"
+FOLDER="7_final_200"
 MODEL="~/.config/kraken/arabic-beirut-200.clstm"
 
 python $SCRIPTPATH/eval.py $MODEL $SCRIPTPATH/$1/$FOLDER
-cat $SCRIPTPATH/$1/$FOLDER/*.gt.txt > $SCRIPTPATH/$1/g_$VAR.txt
-cat $SCRIPTPATH/$1/$FOLDER/*.rec.txt > $SCRIPTPATH/$1/r_$VAR.txt
-ocrevalutf8 accuracy $SCRIPTPATH/$1/g_$VAR.txt $SCRIPTPATH/$1/r_$VAR.txt > $SCRIPTPATH/$1/report_$VAR.txt
+cat $SCRIPTPATH/$1/$FOLDER/*.gt.txt > $SCRIPTPATH/$1/g$VAR.txt
+cat $SCRIPTPATH/$1/$FOLDER/*.rec.txt > $SCRIPTPATH/$1/r$VAR.txt
+ocrevalutf8 accuracy $SCRIPTPATH/$1/g$VAR.txt $SCRIPTPATH/$1/r$VAR.txt > $SCRIPTPATH/$1/report$VAR.txt
 echo $SCRIPTPATH/$1/$FOLDER": folder has been processed!"
 
 echo $SCRIPTPATH/$1": folder has been processed!"
